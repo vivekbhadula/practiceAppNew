0d049239f9e85fc4fdf0ac1c501d7f8c8753273a
vivekbhadula <vivekbhadula201420@gmail.com>
2021-02-18 11:38:43 +0530

    deleted ViewSwift contents.

--- PracticeApp/ViewController.swift
+++ PracticeApp/ViewController.swift
@@ -5,28 +5,5 @@
 //  Created by Vivek Bhadula on 17/02/21.
 //
 
-import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var changeImage: UIImageView!
-    @IBOutlet weak var mainBG: UIImageView!
-    
-    
-    @IBAction func keyPressed(_ sender: UIButton) {
-        changeImage.alpha = 1
-        changeImage.image = [#imageLiteral(resourceName: "practiceApp1"), #imageLiteral(resourceName: "practiceApp2")][Int.random(in: 0...1)]
-    }
-    
-    @IBAction func backButton(_ sender: UIButton) {
-        changeImage.alpha = 0 //fade to 0. Added comment to check if commit shows on github.
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        mainBG.image = #imageLiteral(resourceName: "practiceApp")
-    }
-
-
-}
 
